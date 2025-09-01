0de46a89998713032f6436707ad5eb12ffdf12ab
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-01 22:53:13 +0200

    implemented react-icons

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
-
+import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai';
 const Navbar = () => {
   return (
     <div>
-        
+        <div>
+            <Link href='/'/>
+             <h1>Knitting Toys</h1>
+             <ul>
+                <li>
+                    <Link href='/'> Home</Link> 
+                </li>
+                <li>
+                    <Link href='/#gallery'> Gallery</Link>
+                </li>
+                <li>
+                    <Link href='/portfolio'> Work</Link>
+                </li>
+                <li>
+                    <Link href='/contact'>Contact </Link>
+                </li>
+             </ul>
+
+                     {/* MOBILE BUTTON  */}
+  <div>
+    <AiOutlineMenu size={20} />
+  </div>
+        </div>
     </div>
   )
 }
