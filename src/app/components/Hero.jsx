1537479e43d3b1042dfc9b0a3b32b7d944308d40
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-01 10:23:32 +0200

    created folder and file there.

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+       <div>
+        {/* overlay  */}
+    </div>
+    <div> 
+        <h2>Heading</h2>
+        <p>message</p>
+        <button>Book</button>
+    </div>
+    </>
+ 
+  )
+}
+
+export default Hero
+ 
