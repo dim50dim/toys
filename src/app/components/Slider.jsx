5bec427ce39138816f951709b995912cd43d086d
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-02 10:57:21 +0200

    created two functions nextSlide and prevSlide.

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+ "use client"
+
+import  { useState } from 'react'
 import { SliderData } from './SliderData'
 import Image from 'next/image'
+
+
 const Slider = ({slides}) => {
+    const [current,setCurrent] = useState(0);
+    const length = slides.length;
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+    const prevSlide = () => {
+        setCurrent(current === 0 ?length - 1  : current - 1)
+    }
   return (
     <div>
          <div id='gallery'>
@@ -9,13 +21,17 @@ const Slider = ({slides}) => {
                <div>
                   {SliderData.map((slide,index) => {
                     return (
-                        <Image 
+
+                   <div  className=''>
+                         <Image 
                          style={{objectFit:'cover'}} 
-                          key={index} src={slide.image} 
+                          key={index} 
+                          src={slide.image} 
                           alt="/slide"
                            width='1440'
                             height='600'
                           />
+                   </div>
                     )
                   })}
                </div>
