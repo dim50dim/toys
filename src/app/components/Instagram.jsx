be5f2093cb7c5583f49a396d36def36fb38626d7
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-03 08:21:35 +0200

    implemented the same photo , just for example.

--- src/app/components/Instagram.jsx
+++ src/app/components/Instagram.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import IdImag1 from '../public/images/slider/p_2.jpg'
+import IdImag1 from '../public/images/slider/p_2.jpg'
+import IdImag1 from '../public/images/slider/p_2.jpg'
+import IdImag1 from '../public/images/slider/p_2.jpg'
+import IdImag1 from '../public/images/slider/p_2.jpg'
+import InstagramImg from './InstagramImg'
 const Instagram = () => {
   return (
     <div>
-      
+         <p>Follow me on Instagram</p>
+         <p>@Knitting Toys</p>
+           <div>
+               <InstagramImg/>
+           </div>
     </div>
   )
 }
