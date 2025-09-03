a651c469ac4604ec93b0457fd332071101a5be6f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-03 09:33:58 +0200

    styled component with FaInstagram.

--- src/app/components/Instagram.jsx
+++ src/app/components/Instagram.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 
 
 import InstagramImg from './InstagramImg';
-import IdImag1 from "../../app/../../public/images/slider/p_2.jpg"
+import IdImag2 from "../../app/../../public/images/slider/p_2.jpg"
+import IdImag1 from "../../app/../../public/images/slider/p_1.jpg"
+import IdImag3 from "../../app/../../public/images/slider/p_3.jpg"
+import IdImag4 from "../../app/../../public/images/slider/p_4.jpg"
+import IdImag5 from "../../app/../../public/images/slider/p_5.jpg"
+import IdImag6 from "../../app/../../public/images/slider/p_6.jpg"
 const Instagram = () => {
   return (
-    <div>
-         <p>Follow me on Instagram</p>
-         <p>@Knitting Toys</p>
-           <div>
+    <div className='max-w-[1240px] mx-auto text-center py-24'>
+         <p className='text-2xl font-bold'>Follow me on Instagram</p>
+         <p className='pb-4'>@Knitting Toys</p>
+           <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
                <InstagramImg socialImg={IdImag1}/>
+               <InstagramImg socialImg={IdImag2}/>
+               <InstagramImg socialImg={IdImag3}/>
+               <InstagramImg socialImg={IdImag4}/>
+               <InstagramImg socialImg={IdImag5}/>
+               <InstagramImg socialImg={IdImag6}/>
+ 
+
            </div>
     </div>
   )
--- src/app/components/InstagramImg.jsx
+++ src/app/components/InstagramImg.jsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
 
 const InstagramImg = ({socialImg} ) => {
   return (
-    <div>
-          <Image src={socialImg}/>
+    <div className='relative'>
+          <Image src={socialImg} className='w-full h-full'   alt="insta"/>
+           {/* overlay  */}
+            <div className='group flex justify-center items-center absolute top-0 left-0 right-0 bottom-0 hover:bg-black/70'>
+                <p className='hidden text-gray-300 group-hover:block' >
+                     <FaInstagram size={30} className='z-10'/></p>
+            </div>
     </div>
   )
 }
