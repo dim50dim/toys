f2b0d941986bbaede82d22d309f9de863d7ba1c8
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-04 11:06:18 +0200

    almost finish basic part.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // required for next export
-    domains: ['images.unsplash.com'], // optional, safe to keep
+   
   },
 //   basePath: '/<repo-name>', // only if using GitHub Pages under a repo
 };
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Contact from '../components/Contact'
 
 const contact = ({heading,message}) => {
   return (
     <div>
        <Hero heading="This is my contacts" message="Get in touch" />
+       <Contact/>
     </div>
   )
 }
